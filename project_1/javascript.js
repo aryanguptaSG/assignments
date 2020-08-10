e4c0ac55cd406b0c8f520e32b9ea1868668114da
aryanguptaSG <aryan31102001@gmail.com>
2020-08-10 17:16:11 +0530

    Update javascript.js

--- project_1/javascript.js
+++ project_1/javascript.js
@@ -1,3 +1,17 @@
 var cloud = document.querySelector("#id_cloud").cloneNode(true);
 cloud.id="id1_cloud"
 document.getElementById("main").appendChild(cloud);
+
+
+var flag = document.querySelector(".flag").cloneNode(true);
+flag.id="id1_flag"
+document.getElementById("mountain").appendChild(flag);
+
+var i=1;
+while(i<=4){
+var tree = document.querySelector(".tree").cloneNode(true);
+tree.id="id"+i+"_tree"
+document.getElementById("mountain").appendChild(tree);
+i+=1;
+
+}
