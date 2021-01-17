88ef622bd8341da62f3214d04c4cb3359b2247d9
aryanguptaSG <aryan31102001@gmail.com>
2021-01-17 19:38:54 +0530

    Create javascript.js

---
+++ letscode/javascript.js
@@ -0,0 +1,27 @@
+document.getElementById("navicon1").addEventListener('mouseover',function(){
+    document.getElementById('lable1').style.display="inline-flex";
+})
+document.getElementById("navicon1").addEventListener('mouseout',function(){
+    document.getElementById('lable1').style.display="none";
+})
+
+document.getElementById("navicon2").addEventListener('mouseover',function(){
+    document.getElementById('lable2').style.display="inline-flex";
+})
+document.getElementById("navicon2").addEventListener('mouseout',function(){
+    document.getElementById('lable2').style.display="none";
+})
+
+document.getElementById("navicon3").addEventListener('mouseover',function(){
+    document.getElementById('lable3').style.display="inline-flex";
+})
+document.getElementById("navicon3").addEventListener('mouseout',function(){
+    document.getElementById('lable3').style.display="none";
+})
+
+document.getElementById("navicon4").addEventListener('mouseover',function(){
+    document.getElementById('lable4').style.display="inline-flex";
+})
+document.getElementById("navicon4").addEventListener('mouseout',function(){
+    document.getElementById('lable4').style.display="none";
+})
