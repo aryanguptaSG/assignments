a078e6c0f753d1a39dbe95ed5d94ca52f8c33375
aryanguptaSG <aryan31102001@gmail.com>
2021-02-09 09:40:52 +0530

    selection sort included

--- algorithms_visualiation/javascript.js
+++ algorithms_visualiation/javascript.js
@@ -5,7 +5,7 @@ function creatbar(){
  for(var i=1;i<w/10;i++){
      var bar= document.getElementById("bar0").cloneNode(true);
      bar.id = "bar"+i;
-     bar.style.height = String(Math.floor(Math.random()*height))+"px";
+     bar.style.height = String(Math.floor(Math.random()*height-10))+"px";
      document.getElementById("visual").appendChild(bar)
  }     
 }
@@ -14,9 +14,25 @@ creatbar()
 function sleep(delay){
     return new Promise(resolve=>setTimeout(resolve,delay));
 }
+async function recolour(n){
+    for(var i=0;i<n;i++){
+        document.getElementById("bar"+i).style.backgroundColor="rgb(3, 144, 130)";
+        await sleep(50);
+    }
+}
+function deactivate(n){
+    if(n==1){
+    document.getElementById("controles").style.opacity="0";
+}
+else{
+        document.getElementById("controles").style.opacity="1";
+}
+}
+
+
 
 async function Bubblesort(){
-    document.getElementById("bubble").onclick="";
+    deactivate(1);
     var array = document.getElementById("visual")
     var n= array.childElementCount
     console.log(n+" Numbers of element")
@@ -39,10 +55,45 @@ async function Bubblesort(){
 
         }
     }
-
-    for(var i=0;i<n;i++){
-        document.getElementById("bar"+i).style.backgroundColor="rgb(3, 144, 130)";
-        await sleep(50);
-    }
-    document.getElementById("bubble").onclick="Bubblesort()";
+    recolour(n);
+    deactivate(0);
 }
+
+
+async function selectionSort()  
+{ deactivate(1);
+    var array = document.getElementById("visual")
+    var n= array.childElementCount
+    console.log(n+" Numbers of element")  
+  
+    // One by one move boundary of unsorted subarray  
+    for (var i = 0; i<n-1; i++)  
+    {  
+        // Find the minimum element in unsorted array  
+       var  min_idx = i;  
+       var h1=document.getElementById("bar"+min_idx).offsetHeight;
+       var min_h=h1;
+        for (var j=i+1;j<n;j++) { 
+            document.getElementById("bar"+i).style.backgroundColor="red";
+            document.getElementById("bar"+j).style.backgroundColor="green";
+            var h2=document.getElementById("bar"+j).offsetHeight;
+            console.log(h1,h2);
+            await sleep(50);
+            if (h2 < min_h ) { 
+                min_idx = j;
+                min_h=h2; 
+            } 
+            document.getElementById("bar"+j).style.backgroundColor="rebeccapurple";
+            document.getElementById("bar"+i).style.backgroundColor="rebeccapurple";
+        }
+        document.getElementById("bar"+min_idx).style.height=h1+"px";
+        document.getElementById("bar"+i).style.height=min_h+"px";
+
+
+        // Swap the found minimum element with the first element  
+         
+    } 
+    
+    recolour(n);
+    deactivate(0);
+} 
